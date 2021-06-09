002c37bc4d9c4f79dad7a67760d6c822eecaf4bb
keilaviana <keilavg72@gmail.com>
2021-06-09 17:49:25 -0300

    adicionando função para dinamizar os ID dos pokemons

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // Função que irá buscar os pokemons
 
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/25`
-    // função que faz requisições AJAX no browser
-    fetch(url)
-    // função que vai retornar a resposta da promisse convertida em json, para obtermos o body
-        .then(response => response.json())
-        // visualizar a informação recebida por parâmetro
-        .then(pokemon => {
-            console.log(pokemon)
-        })
+    //função para pegar id dinamico do pokemon
+    const getPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    // a cada execução desse loop a promess na qual a expressão está resultando seja adicionada a um array de promess
+    for (let i = 1; i <= 150; i++){
+        fetch(getPokemonUrl(i)).then(response => response.json())
+    }
+    // // função que faz requisições AJAX no browser
+    // fetch(url)
+    // // função que vai retornar a resposta da promisse convertida em json, para obtermos o body
+    //     .then(response => response.json())
+    //     // visualizar a informação recebida por parâmetro
+    //     .then(pokemon => {
+    //         console.log(pokemon)
+    //     })
 }
 
 fetchPokemon()
